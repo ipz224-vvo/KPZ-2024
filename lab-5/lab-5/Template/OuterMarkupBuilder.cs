f2c711287bdd1d317e00a8fd82dc5bc00d6dc498
ipz224_vvo <ipz224_vvo@student.ztu.edu.ua>
2024-05-26 21:20:46 +0300

    Added interface of builder and HTML builder

---
+++ lab-5/lab-5/Template/OuterMarkupBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using lab_5.LightHTML;
+
+namespace lab_5.Template;
+
+interface OuterMarkupBuilder
+{
+    void AddStartTag(string startTag);
+    void AddId(Guid guid);
+    void AddCssClasses(List<string> cssClasses);
+    void AddInnerMarkup(List<LightNode> childs);
+    void AddEventListeners(EventSubscription eventSubcriptions);
+    string BuildMarkup(string tagName, Guid id, List<string> cssClasses, List<LightNode> childs,
+        ClosingType closingType, EventSubscription eventSubscription);
+}
